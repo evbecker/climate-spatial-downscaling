35aa6b27b8f721886aeac4441d058ebf4e6c0f5b
Evan Becker <evbecker@ucla.edu>
2022-03-13 22:53:53 -0700

    added general dataloader that also works with temperature

--- dataloader.py
+++ dataloader.py
@@ -246,6 +246,94 @@ class EraiCpcWrfDataset(Dataset):
     def __len__(self):
         return self.data_len
 
+class EraiCpcWrfDataset_General(Dataset):
+
+    def __init__(self, img_path, mode='train', regions=['neus','nwus','seus','swus'], var_type='precip'):
+        self.var_type = var_type
+        self.img_path = img_path
+        self.erai=[]
+        self.cpc=[]
+        self.wrf=[]
+        self.regions=[]
+        self.elevation={}
+        self.latitude={}
+        self.longitude={}
+        if mode!='new_test':
+            for r in regions:
+                if mode=='train':
+                    date='2003-01-01'
+                    end_date='3000-01-01'
+                elif mode=='val':
+                    date='2002-01-01'
+                    end_date='2003-01-01'
+                elif mode=='test':
+                    date='2001-01-01'
+                    end_date='2002-01-01'
+                while date!=end_date:
+                    if os.path.exists(img_path+'/erai-'+r+f'-{var_type}-'+date+'.pt'):
+                        self.erai.append('erai-'+r+f'-{var_type}-'+date+'.pt')
+                    if os.path.exists(img_path+'/cpc-'+r+f'-{var_type}-'+date+'.pt'):
+                        self.cpc.append('cpc-'+r+f'-{var_type}-'+date+'.pt')
+                    if os.path.exists(img_path+'/wrf-'+r+'-precip-'+date+'.pt'):
+                        self.wrf.append('wrf-'+r+'-precip-'+date+'.pt')
+                    self.regions.append(r)
+                    date=next_date(date)
+                self.elevation[r]=torch.load(img_path+'/'+r+'-elevation-160x160.pt')
+            self.latitude['nwus']=torch.linspace(38,48,160).unsqueeze(1)
+            self.longitude['nwus']=torch.linspace(238,248,160).unsqueeze(0)
+            self.latitude['seus']=torch.linspace(28,38,160).unsqueeze(1)
+            self.longitude['seus']=torch.linspace(268,278,160).unsqueeze(0)
+            self.latitude['neus']=torch.linspace(35,45,160).unsqueeze(1)
+            self.longitude['neus']=torch.linspace(273,283,160).unsqueeze(0)
+            self.latitude['swus']=torch.linspace(32,42,160).unsqueeze(1)
+            self.longitude['swus']=torch.linspace(242,252,160).unsqueeze(0)
+        else:
+            date='2001-01-01'
+            end_date='3000-01-01'
+            r='mwus'
+            #print(os.path.exists(img_path+'/erai-'+r+'-{var_type}-'+date+'.pt'),os.path.exists(img_path+'/cpc-'+r+'-{var_type}-'+date+'.pt'),os.path.exists(img_path+'/wrf-'+r+'-{var_type}-'+date+'.pt'))
+            while os.path.exists(img_path+'/erai-'+r+f'-{var_type}-'+date+'.pt') and os.path.exists(img_path+'/cpc-'+r+f'-{var_type}-'+date+'.pt') and os.path.exists(img_path+'/wrf-'+r+'-{var_type}-'+date+'.pt') and date!=end_date:
+                self.erai.append('erai-'+r+f'-{var_type}-'+date+'.pt')
+                self.cpc.append('cpc-'+r+f'-{var_type}-'+date+'.pt')
+                self.wrf.append('wrf-'+r+f'-{var_type}-'+date+'.pt')
+                self.regions.append(r)
+                date=next_date(date)
+            self.elevation[r]=torch.load(img_path+'/'+r+'-elevation-160x160.pt')
+            self.latitude['mwus']=torch.linspace(35,45,160).unsqueeze(1)
+            self.longitude['mwus']=torch.linspace(255,265,160).unsqueeze(0)
+
+
+
+        #self.cpc = np.asarray(self.data_info.iloc[1])
+        # length is number of time samples
+        self.data_len = len(self.erai)
+
+    def __getitem__(self, index):
+        # for index corresponding to time t, returns high and low
+        # resolution images for that time, as well as high res
+        # at the previous timestep
+        slr_img = torch.load(os.path.join(self.img_path, self.erai[index])).unsqueeze(0)
+        if self.var_type == 'precip':
+            lr_img = torch.load(os.path.join(self.img_path, self.cpc[index])).unsqueeze(0)
+        else:
+            lr_img = torch.zeros((1,160,160))
+        hr_img=torch.load(os.path.join(self.img_path, self.wrf[index])).unsqueeze(0)
+        prev_index = (index - 1)%self.data_len
+        prev_hr_img = torch.load(os.path.join(self.img_path, self.wrf[prev_index])).unsqueeze(0)
+        coord=torch.zeros((4,slr_img.size(1),slr_img.size(2)))
+        r=self.regions[index]
+        coord[0,:,:]=date_converter(self.erai[index][-13:-3])
+        coord[1,:,:]=self.latitude[r]
+        coord[2,:,:]=self.longitude[r]
+        coord[3,:,:]=self.elevation[r]
+        #print(self.erai[index][-13:-3])
+        return {'hr_img':hr_img.type(torch.FloatTensor),'lr_img':lr_img.type(torch.FloatTensor),
+                'prev_hr_img':prev_hr_img.type(torch.FloatTensor),'coord':coord,'name':self.erai[index],
+                'region':r,'slr_img':slr_img.type(torch.FloatTensor)}
+
+    def __len__(self):
+        return self.data_len
+
 if __name__ == "__main__":
     # just testing that we return three tensor images for a given index
     #print(torch.load('./tensordata/erai-nwus-precip-2000-01-02.pt'))
