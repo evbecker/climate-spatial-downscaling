e08cd97a297796f51bb42087eb90ea51fbbc0d99
Ted Zadouri <tedzadouri@gmail.com>
2022-03-13 23:17:08 -0700

    Add files via upload

--- numerical_analysis.py
+++ numerical_analysis.py
@@ -89,7 +89,4 @@ for i in [0.01,0.02,0.05,0.1,0.2,0.5,1,2,5,10]:
     evaluate('EAD','generator'+str(i)+'_.pt')
 '''
 #evaluate('AE','autoencoder_ae.pt')
-evaluate('ours','_generatator10.pt',160)
-
-
-
+evaluate('ours','_generator10.pt',160)
